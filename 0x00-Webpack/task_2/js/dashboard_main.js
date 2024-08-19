5ddbda592319889a9aee473139b008ce0ba7896c
Madani00 <mamchart@gmail.com>
2024-08-19 23:15:40 +0100

    task 2

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+
+let count = 0;
+
+$('body').append('<div id="logo"></div>');
+$("<p>Holberton Dashboard</p>").appendTo('body');
+$("<p>Dashboard data for the students</p>").appendTo('body');
+$("<button>Click here to get started</button>").appendTo('body');
+$("<p id='count'></p>").appendTo('body');
+$("<p>Copyright - Holberton School</p>").appendTo('body');
+
+function updateCounter() {
+    count++;
+    $('#count').html(`${count} clicks on the button`);
+}
+
+$("button").on("click", _.debounce(updateCounter, 500));
