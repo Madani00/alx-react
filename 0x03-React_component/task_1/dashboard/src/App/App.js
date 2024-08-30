ff3ae8320b5f6c8912bcd93b591c2e44616d0d76
Madani00 <mamchart@gmail.com>
2024-08-30 01:55:16 +0100

    lifecycles

---
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import Notif from "../Notifications/Notifications";
+import "./App.css";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.keyressHandler = this.keyressHandler.bind(this);
+  }
+
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+
+  listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotification() },
+  ];
+
+  keyressHandler(e) {
+    if (e.ctrlKey && e.key === "h") {
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  }
+  componentDidMount() {
+    document.addEventListener("keydown", this.keyressHandler);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.keyressHandler);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Notif listNotifications={this.listNotifications} />
+        <div className="App">
+          <Header />
+          <div className="App-body">
+            {this.props.isLoggedIn ? (
+              <CourseList listCourses={this.listCourses} />
+            ) : (
+              <Login />
+            )
+            }
+          </div>
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {
+    return;
+  },
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+export default App;
